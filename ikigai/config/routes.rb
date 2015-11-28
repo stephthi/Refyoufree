87995fc73f812a9bdd4aaf319a1ea6b53998c697
stephthi <nstephanie2@gmail.com>
2015-11-28 00:43:47 -0500

    Added application model

--- ikigai/config/routes.rb
+++ ikigai/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:new, :create, :destroy, :show]
+  root 'users#new'
 
+  resources :users, only: [:new, :create, :destroy, :show]
+  resources :sessions, only: [:new, :creeate, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
