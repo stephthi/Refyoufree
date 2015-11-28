2df62d467c068c2e3f37e0fd86062a3f5c33445d
stephthi <nstephanie2@gmail.com>
2015-11-28 04:13:05 -0500

    User information saving

--- ikigai/app/controllers/users_controller.rb
+++ ikigai/app/controllers/users_controller.rb
@@ -6,7 +6,12 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to '../sessions/new', notice: "Signed up!"
+    else
+      render "new"
+    end
   end
 
   def show
