b306caf826625212f7adfd7d5956ae75e28c7a92
stephthi <nstephanie2@gmail.com>
2015-11-28 22:49:54 -0500

    Newest commit with front end

--- ikigai/app/controllers/sessions_controller.rb
+++ ikigai/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/documentations/new', notice: "Logged In!"
+      redirect_to '../documentations/new', notice: "Logged In!"
     else
       flash.now[:alert] = "Invalid email or password"
       render "new"
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to documentations_url, notice: "Logged out!"
+    redirect_to '../documentations/new', notice: "Logged out!"
   end
 end
